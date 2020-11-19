f1b822daaa437cdb1045caacf7cd856b0d577f6a
Jason Corderoy <jcorderoy@woolworths.com.au>
2020-11-19 18:00:23 +0000

    model to html output added.

--- scripts/main.py
+++ scripts/main.py
@@ -17,11 +17,11 @@ project = "gcp-wow-finance-de-lab-dev"
 client = bigquery.Client(location=location, project=project)
 print("Client creating using default project: {}".format(client.project))
 
-run4salesOrgs = ['1005','1030']
+run4salesOrgs = ['1005']
 
 # Create the main elasticity data table in gcp?
 # no need to recreate the elasticity data every time.
-if (0):
+if (1):
     run_bq(sql=bq_sql_mainData(so=run4salesOrgs, lagDays=7),
            client=client,
            location=location)
@@ -82,20 +82,27 @@ for i in np.unique(df_site_cluster_pair.cluster):
     i_sites = list(
         df_site_cluster_pair[df_site_cluster_pair.cluster == i]['site'])
     dat = get_bq_data("""
-    select
-    Article, Sales_Unit,
-    log_ASP_v_lag,
-    log_Sales_Qty_SUoM_v_lag 
-    from `gcp-wow-finance-de-lab-dev.price_elasticity.elastData`
+    SELECT a.Article, a.Sales_Unit, a.log_ASP_v_lag, a.log_Sales_Qty_SUoM_v_lag
+    FROM `gcp-wow-finance-de-lab-dev.price_elasticity.elastData` a inner join
+    (
+    select Site, Article, Sales_Unit, count(*) as n
+    from`gcp-wow-finance-de-lab-dev.price_elasticity.elastData`
     where abs(log_ASP_v_lag) > 0.1 and  Site in ('{i_s}')
+    group by Site, Article, Sales_Unit
+    ) b
+    on (a.Article=b.Article) and (a.Sales_Unit=b.Sales_Unit) and (a.Site=b.Site)
+    where b.n > 20
     """.format(i_s="', '".join(i_sites)),
                       project_id=project)
     models_gb = ['Article', 'Sales_Unit']
     dict_modelData = dict(iter(dat.groupby(models_gb)))
     for key, value in dict_modelData.items():
-        dict_all_models[key] = run_lm(
-            dict_modelData[key]['log_ASP_v_lag'],
-            dict_modelData[key]['log_Sales_Qty_SUoM_v_lag'])
+        dict_all_models[key] = pd.read_html(
+            run_lm(
+                dict_modelData[key]['log_ASP_v_lag'],
+                dict_modelData[key]['log_Sales_Qty_SUoM_v_lag']
+            ).summary().tables[1].as_html(), header=0, index_col=0)[0]
+
     fname = "_".join(run4salesOrgs) + "_cluster_" + str(
         i) + "_dict_cluster_models.pickle"
     save_pickle_to_gStorage(obj=dict_all_models,
@@ -106,44 +113,3 @@ for i in np.unique(df_site_cluster_pair.cluster):
     list_bucket_files.append(fname)
     # dict_cluster_models[i] = dict_all_models
 
-for i in range(len(list_bucket_files)):
-    fname = list_bucket_files[i]
-    print(fname)
-    bb = bucket.blob(fname)
-    bb.download_to_filename(fname)
-    file_to_read = open(fname, "rb")
-    tempDict = pickle.load(file_to_read)
-    df_models = pd.DataFrame.from_dict(tempDict.keys()).reset_index()
-    df_models.columns = ['index_x'] + models_gb
-    df_models = df_models.drop(columns='index_x')
-    df_models['cluster'] = re.search(".*cluster_(.*)_dict_.*", fname,
-                                     re.IGNORECASE).group(1)
-    df_models['intercept'] = [tempDict[x].params[0] for x in tempDict.keys()]
-    df_models['coef_log_ASP_v_lag7'] = [
-        tempDict[x].params[1] for x in tempDict.keys()
-    ]
-    df_models['conf_int_025_intercept'] = [
-        tempDict[x].conf_int(alpha=0.05).iloc[0, 0] for x in tempDict.keys()
-    ]
-    df_models['conf_int_975_intercept'] = [
-        tempDict[x].conf_int(alpha=0.05).iloc[0, 1] for x in tempDict.keys()
-    ]
-    df_models['conf_int_025_coef_log_ASP_v_lag7'] = [
-        tempDict[x].conf_int(alpha=0.05).iloc[1, 0] for x in tempDict.keys()
-    ]
-    df_models['conf_int_975_coef_log_ASP_v_lag7'] = [
-        tempDict[x].conf_int(alpha=0.05).iloc[1, 1] for x in tempDict.keys()
-    ]
-    if i == 0:
-        df_all_models = df_models
-    else:
-        df_all_models = pd.concat([df_all_models, df_models])
-    os.system("rm {}".format(fname))
-
-save_pickle_to_gStorage(obj=df_all_models,
-                        pickle_fname="_".join(run4salesOrgs) +
-                        "_df_all_models.pickle",
-                        bucket=bucket)
-
-write_df_to_bq(df_all_models, "price_elasticity.cluster_elasticity",
-               "gcp-wow-finance-de-lab-dev")
